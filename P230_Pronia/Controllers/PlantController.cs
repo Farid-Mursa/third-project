36232e0b26c9b147619d58621267f59b18f6080e
Farid-Mursa <faridfmur@code.edu.az>
2023-03-26 12:51:56 +0400

    Pronia

---
+++ P230_Pronia/Controllers/PlantController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+using P230_Pronia.Entities;
+
+namespace P230_Pronia.Controllers
+{
+    public class PlantController : Controller
+    {
+        private readonly ProniaDbContext _context;
+
+        public PlantController(ProniaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Detail(int id)
+        {
+            if (id == 0) return NotFound();
+            Plant? plant = _context.Plants
+                                .Include(p=>p.PlantImages)
+                                    .Include(p=>p.PlantDeliveryInformation)
+                                        .Include(p=>p.PlantTags)
+                                            .ThenInclude(pt=>pt.Tag)
+                                                .Include(p=>p.PlantCategories)
+                                                    .ThenInclude(pc=>pc.Category).FirstOrDefault(p => p.Id == id);
+            
+            List<int> saleProducts = _context.Plants.Select(p => p.Id).ToList();
+
+            ViewBag.RelatedPlant = _context.Plants
+                .Include(p => p.PlantImages)
+                .Include (p=>p.PlantCategories)
+                .ThenInclude(p => p.Category)
+                .Where(p => p.PlantCategories.Any(p => saleProducts.Contains(p.Category.Id)))
+
+                .Take(4)
+                .ToList();
+            if (plant is null) return NotFound();
+            return View(plant);
+        }
+        
+    }
+}
